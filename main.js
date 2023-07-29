a05311b47355779b010ff451649ef7345a924393
Live Blogger <61611286+livebloggerofficial@users.noreply.github.com>
2023-07-30 01:45:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const labels = document.querySelectorAll(".custom-sidebar .label");
 labels.forEach((label, index) => {
   label.style.transitionDelay = `${index * 50}ms`;
 });
+
+const sidebarBtn = document.querySelector(".custom-sidebar .sidebar-btn");
+const customSidebar = document.querySelector(".custom-sidebar");
+
+sidebarBtn.addEventListener("click", () => {
+  customSidebar.classList.toggle("active");
+});
+
